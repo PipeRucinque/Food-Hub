a6e9eeb458781bca553b193f70da8270acbb3802
Pipe Rucinque <luis.felipe.rucinque@gmail.com>
2023-01-10 10:08:04 +0100

    navBar enhance it, appear one thing or another depending on state true/false (logged in or not), and remove de hamburger menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,37 +1,38 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {Container, Form, Nav, Navbar, NavDropdown, Offcanvas} from 'react-bootstrap/';
-import { NavLink, Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const styles = {
   color: "white",
 }
 
  const Header = () => {
+  const [terms, setTerms] = useState(false)
+
   return (
     <>
         <Navbar key="md" bg="dark" expand="md" className="mb-3">
           <Container fluid>
             <Navbar.Brand href="/" style={styles}>Logo Food_Hub</Navbar.Brand>
-            <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-md`} />
-            <Navbar.Offcanvas
-              id={`offcanvasNavbar-expand-md`}
-              aria-labelledby={`offcanvasNavbarLabel-expand-md`}
-              placement="end"
-            >
-              <Offcanvas.Header closeButton>
-                <Offcanvas.Title id={`offcanvasNavbarLabel-expand-md`}>
-                  Food_Hub Menu
-                </Offcanvas.Title>
-              </Offcanvas.Header>
-              <Offcanvas.Body>
-                <Nav className="justify-content-end flex-grow-1 pe-3">
-                  <NavLink className="nav-link" to="/map" style={styles}>Map</NavLink>
-                  <NavLink className="nav-link" to="/categories" style={styles}>Categories</NavLink>
-                  <NavLink className="nav-link" to="/landings" style={styles}>Log In</NavLink>
-                  <NavLink className="nav-link" to="/register" style={styles}>Register</NavLink>
-                </Nav>
-              </Offcanvas.Body>
-            </Navbar.Offcanvas>
+            <Form.Check 
+                        type="checkbox" 
+                        label="True/False"
+                        value={terms} 
+                        onChange={(e) => setTerms(e.target.checked)}
+                        style={{color: 'white'}}
+                    />
+            <Nav className="justify-content-end flex-grow-1 pe-3">
+              <NavLink className="nav-link" to="/map" style={styles}>Map</NavLink>
+              <NavLink className="nav-link" to="/categories" style={styles}>Categories</NavLink>
+              {!terms 
+                ? <NavLink className="nav-link" to="/register" style={styles}>Register</NavLink>
+                : <NavLink className="nav-link" to="#" style={styles}>Favorites</NavLink>
+              }
+              {!terms 
+                ? <NavLink className="nav-link" to="#" style={styles}>Log In</NavLink>
+                : <NavLink className="nav-link" to="#" style={styles}>Log Out</NavLink> 
+              } 
+            </Nav>
           </Container>
         </Navbar>
     </>
