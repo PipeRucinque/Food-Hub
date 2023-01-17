1a1ed31b28987e95ed5936736831f0f5a7efb37b
Pipe Rucinque <luis.felipe.rucinque@gmail.com>
2023-01-17 10:27:53 +0100

    log state complete, using useContext and localStorege(key=storeLogged)

--- client/src/layouts/Layouts.js
+++ client/src/layouts/Layouts.js
@@ -1,15 +1,16 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { Outlet } from 'react-router-dom'
 import Header from '../components/Header'
+import LoggedContext from '../context/LoggedContext'
 
 function Layouts() {
-
+  const [isLogged, setIsLogged] = useState(false)
   return (
-  <>
-      <Header />
+    <LoggedContext.Provider value={[isLogged, setIsLogged]}>
+      <Header/>
       <Outlet/>
       {/* <Footer /> */}
-  </>
+    </LoggedContext.Provider>
   )
 }
 
